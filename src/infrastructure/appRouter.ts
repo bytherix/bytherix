8026829255b6d73ed0757715520253151de0928c
raaz-srijan <srijannospace@gmail.com>
2026-08-25 19:30:35 +0545

    feat(payment): register payment routes

--- src/infrastructure/appRouter.ts
+++ src/infrastructure/appRouter.ts
@@ -11,6 +11,7 @@ import userRoute from "../modules/user/user.route.js";
 import instructorRoute from "../modules/instructor/instructor.route.js";
 import userProfileRoute from "../modules/user-profile/userProfile.route.js";
 import enrollmentRoute from "../modules/enrollment/enrollment.route.js";
+import paymentRoute from "../modules/payment/payment.route.js";
 
 const appRouter = Router();
 
@@ -25,5 +26,6 @@ appRouter.use("/users", userRoute);
 appRouter.use("/instructors", instructorRoute);
 appRouter.use("/user-profiles", userProfileRoute);
 appRouter.use("/enrollments", enrollmentRoute);
+appRouter.use("/payments", paymentRoute);
 
 export default appRouter;
